b703f0a3b2a0c2baea0501c323e6bafce3673cc8
MurthyARS <60318333+MurthyARS@users.noreply.github.com>
2020-07-26 23:01:40 +0530

    Add files via upload

---
+++ Junit/src/main/java/EPAM/Junit/Hello.java
@@ -0,0 +1,12 @@
+package EPAM.Junit;
+/**
+ * Hello world!
+ *
+ */
+public class Hello 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
